607156a5a0b242ccc838a47a5dc080badcd3a368
limyel <limyel@outlook.com>
2025-07-31 14:42:08 +0800

    refactor(common-core-starter):优化 ObjectMapper 配置
    -移除了 JavaTimeModule 的导入，改用自定义的 LocalDateTimeModule- 调整了时区设置，直接使用 "Asia/Shanghai" 时区
    - 优化了 ObjectMapper 的配置顺序和结构- 注释掉了一些未使用的代码行，提高代码可读性

--- linn-common/common-core-starter/src/main/java/com/limyel/linn/common/core/autoconfig/JacksonAutoConfig.java
+++ linn-common/common-core-starter/src/main/java/com/limyel/linn/common/core/autoconfig/JacksonAutoConfig.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.core.json.JsonReadFeature;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.limyel.linn.common.core.utils.json.LocalDateTimeModule;
 import com.limyel.linn.common.core.utils.local.DateFormatUtils;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
@@ -15,8 +14,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
-import java.text.SimpleDateFormat;
-import java.time.ZoneId;
 import java.util.Locale;
 import java.util.TimeZone;
 
@@ -30,26 +27,21 @@ public class JacksonAutoConfig {
         ObjectMapper objectMapper = builder.createXmlMapper(false).build();
         // 设置地点为中国
         objectMapper.setLocale(Locale.CHINA);
-        // 去掉默认时间戳格式
-        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        // 设置为中国上海时区
-        objectMapper.setTimeZone(TimeZone.getTimeZone(ZoneId.systemDefault()));
-        // 序列化时，日期的统一格式
-        objectMapper.setDateFormat(new SimpleDateFormat(DateFormatUtils.DEFAULT_DATETIME_FORMAT, Locale.CHINA));
-        // 序列化处理
-        objectMapper.configure(JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS.mappedFeature(), true);
-        objectMapper.configure(JsonReadFeature.ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER.mappedFeature(), true);
-        objectMapper.findAndRegisterModules();
         // 失败处理
         objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        // 针对 JDK8 时间类的序列化和反序列化
+        objectMapper.registerModule(new LocalDateTimeModule());
+        // 设置为中国上海时区
+        objectMapper.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
+        // 序列化处理
+        objectMapper.configure(JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS.mappedFeature(), true);
+        objectMapper.configure(JsonReadFeature.ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER.mappedFeature(), true);
         // 单引号处理
         objectMapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
-        // 反序列化时，属性不存在的兼容处理
-        objectMapper.getDeserializationConfig().withoutFeatures(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        // 日期格式化
-        objectMapper.registerModule(new LocalDateTimeModule());
-        objectMapper.findAndRegisterModules();
+
+        // 去掉默认时间戳格式
+        // objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 
         return objectMapper;
     }
