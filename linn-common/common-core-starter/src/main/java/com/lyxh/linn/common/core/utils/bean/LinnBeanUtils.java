abe7d5d58eee9861eb7dd791db98e041c14da6c1
limyel <limyel@outlook.com>
2025-09-08 09:49:33 +0800

    feat: 添加 LinnBeanUtils Bean 复制工具类

---
+++ linn-common/common-core-starter/src/main/java/com/lyxh/linn/common/core/utils/bean/LinnBeanUtils.java
@@ -0,0 +1,50 @@
+package com.lyxh.linn.common.core.utils.bean;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+public class LinnBeanUtils {
+
+    public static <T> List<T> copyList(List<?> source, Class<T> targetClass) {
+        if (source == null) {
+            return null;
+        }
+
+        List<T> target = new ArrayList<>();
+
+        for (Object tmpSrc : source) {
+            T t = copyBean(tmpSrc, targetClass);
+            target.add(t);
+        }
+
+        return target;
+    }
+
+    public static <T> T copyBean(Object source, Class<T> targetClass) {
+        try {
+            T target = targetClass.getDeclaredConstructor().newInstance();
+            BeanUtils.copyProperties(source, target);
+            return target;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> List<T> deepCopy(List<T> source) throws IOException, ClassNotFoundException {
+        ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+        ObjectOutputStream out = new ObjectOutputStream(byteOut);
+        out.writeObject(source);
+
+        ByteArrayInputStream byteIn = new ByteArrayInputStream(byteOut.toByteArray());
+        ObjectInputStream in = new ObjectInputStream(byteIn);
+        @SuppressWarnings("unchecked")
+        List<T> r = (List<T>) in.readObject();
+        return r;
+    }
+
+}
