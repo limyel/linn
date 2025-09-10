81586b3c02c0f7b96b2cd9c7435fb738b9e56251
limyel <limyel@outlook.com>
2025-09-10 13:54:35 +0800

    refactor(linn-sys):优化DeptDO类的导入
    - 添加了StatusEnum的导入，以便在DeptDO类中使用状态枚举

--- linn-modules/linn-sys/linn-sys-biz/src/main/java/com/lyxh/linn/sys/dataobject/DeptDO.java
+++ linn-modules/linn-sys/linn-sys-biz/src/main/java/com/lyxh/linn/sys/dataobject/DeptDO.java
@@ -2,6 +2,7 @@ package com.lyxh.linn.sys.dataobject;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.lyxh.linn.common.mybatis.pojo.BaseDO;
+import com.lyxh.linn.sys.api.constant.StatusEnum;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
