9f20f126d615edab2aaefd1cc33fd52e13c8ea93
limyel <limyel@outlook.com>
2025-09-10 13:42:54 +0800

    feat(db): 初始化系统表结构- 新增角色菜单关联表、部门表、字典类型表、字典数据表、系统参数表、登录日志表和通知公告表 - 修改角色表默认类型为 custom

--- linn-startup/src/main/resources/db/migration/V0_1__Init_Sys_Tables.sql
+++ linn-startup/src/main/resources/db/migration/V0_1__Init_Sys_Tables.sql
@@ -31,7 +31,7 @@ CREATE TABLE `sys_role`
     `code`        varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  NOT NULL COMMENT '角色编码',
     `sort`        int                                                           NOT NULL DEFAULT 0 COMMENT '显示顺序',
     `status`      varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  NOT NULL DEFAULT 'normal' COMMENT '状态，normal-正常，abnormal-异常',
-    `type`        varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  NOT NULL DEFAULT 'sys' COMMENT '角色类型，sys-系统内置角色 custom-用户自定义角色',
+    `type`        varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  NOT NULL DEFAULT 'custom' COMMENT '角色类型，sys-系统内置角色 custom-用户自定义角色',
     `remark`      varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '备注',
     `create_by`   bigint NULL     DEFAULT NULL COMMENT '创建者',
     `create_time` datetime                                                      NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
@@ -84,6 +84,98 @@ CREATE TABLE `sys_menu`
   COLLATE = utf8mb4_unicode_ci COMMENT ='菜单表';
 
 
+DROP TABLE IF EXISTS `sys_role_menu`;
+CREATE TABLE `sys_role_menu`
+(
+    `role_id` bigint NOT NULL COMMENT '角色ID',
+    `menu_id` bigint NOT NULL COMMENT '菜单ID',
+    PRIMARY KEY (`role_id`, `menu_id`) USING BTREE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci COMMENT ='角色菜单关联表';
+
+
+DROP TABLE IF EXISTS `sys_dept`;
+CREATE TABLE `sys_dept`
+(
+    `id`          bigint                                                       NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `pid`         bigint                                                       NOT NULL DEFAULT 0 COMMENT '上级 ID',
+    `name`        varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '部门名称',
+    `sort`        int                                                          NOT NULL DEFAULT 0 COMMENT '显示顺序',
+    `leader`      bigint                                                       NOT NULL DEFAULT 0 COMMENT '负责人',
+    `email`       varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '邮箱',
+    `mobile`      varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '手机号',
+    `status`      varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'normal' COMMENT '状态，normal-正常，abnormal-异常',
+    `create_by`   bigint NULL     DEFAULT NULL COMMENT '创建者',
+    `create_time` datetime                                                     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_by`   bigint NULL     DEFAULT NULL COMMENT '更新者',
+    `update_time` datetime                                                     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `delete_time` datetime                                                              DEFAULT NULL COMMENT '删除时间',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci COMMENT ='部门表';
+
+
+DROP TABLE IF EXISTS `sys_dict_type`;
+CREATE TABLE `sys_dict_type`
+(
+    `id`          bigint                                                        NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `dict_name`   varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  NOT NULL COMMENT '字典名称',
+    `dict_type`   varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  NOT NULL COMMENT '字典类型',
+    `remark`      varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '备注',
+    `sort`        int                                                           NOT NULL DEFAULT 0 COMMENT '显示顺序',
+    `create_by`   bigint NULL     DEFAULT NULL COMMENT '创建者',
+    `create_time` datetime                                                      NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_by`   bigint NULL     DEFAULT NULL COMMENT '更新者',
+    `update_time` datetime                                                      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `delete_time` datetime                                                               DEFAULT NULL COMMENT '删除时间',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci COMMENT ='字典类型表';
+
+
+DROP TABLE IF EXISTS `sys_dict_data`;
+CREATE TABLE `sys_dict_data`
+(
+    `id`           bigint                                                         NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `dict_type_id` bigint                                                         NOT NULL DEFAULT 0 COMMENT '字典类型 ID',
+    `dict_label`   varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci   NOT NULL COMMENT '字典标签',
+    `dict_value`   varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '字典值',
+    `remark`       varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  NOT NULL DEFAULT '' COMMENT '备注',
+    `sort`         int                                                            NOT NULL DEFAULT 0 COMMENT '显示顺序',
+    `create_by`    bigint NULL     DEFAULT NULL COMMENT '创建者',
+    `create_time`  datetime                                                       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_by`    bigint NULL     DEFAULT NULL COMMENT '更新者',
+    `update_time`  datetime                                                       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `delete_time`  datetime                                                                DEFAULT NULL COMMENT '删除时间',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci COMMENT ='字典值表';
+
+
+DROP TABLE IF EXISTS `sys_params`;
+CREATE TABLE `sys_params`
+(
+    `id`          bigint                                                         NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `param_label` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci   NOT NULL COMMENT '参数标签',
+    `param_code`  varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci   NOT NULL COMMENT '参数编码',
+    `param_value` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '参数值',
+    `type`        varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci   NOT NULL DEFAULT 'custom' COMMENT '参数类型，sys-系统参数 custom-自定义参数参数',
+    `remark`      varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  NOT NULL DEFAULT '' COMMENT '备注',
+    `create_by`   bigint                                                         NULL     DEFAULT NULL COMMENT '创建者',
+    `create_time` datetime                                                       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_by`   bigint                                                         NULL     DEFAULT NULL COMMENT '更新者',
+    `update_time` datetime                                                       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `delete_time` datetime                                                                DEFAULT NULL COMMENT '删除时间',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci COMMENT ='系统参数表';
+
+
 DROP TABLE IF EXISTS `sys_operation_log`;
 CREATE TABLE `sys_operation_log`
 (
@@ -101,3 +193,41 @@ CREATE TABLE `sys_operation_log`
 ) ENGINE=InnoDB
   DEFAULT CHARSET=utf8mb4
   COLLATE=utf8mb4_unicode_ci COMMENT='操作日志表';
+
+
+DROP TABLE IF EXISTS `sys_log_login`;
+CREATE TABLE `sys_log_login`
+(
+    `id`          bigint                                                        NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `username`    varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  NOT NULL COMMENT '用户名',
+    `ip`          varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '主机地址',
+    `status`      varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'normal' COMMENT '状态，normal-正常，abnormal-异常',
+    `create_by`   bigint                                                        NULL     DEFAULT NULL COMMENT '创建者',
+    `create_time` datetime                                                      NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_by`   bigint                                                        NULL     DEFAULT NULL COMMENT '更新者',
+    `update_time` datetime                                                      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `delete_time` datetime                                                               DEFAULT NULL COMMENT '删除时间',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci COMMENT ='登录日志表';
+
+
+DROP TABLE IF EXISTS `sys_notice`;
+CREATE TABLE `sys_notice`
+(
+    `id`          bigint                                                       NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `title`       varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '标题',
+    `type`        int                                                          NOT NULL COMMENT '公告类型，1-通知 2-公告',
+    `content`     text                                                         NOT NULL COMMENT '内容',
+    `status`      int                                                          NOT NULL DEFAULT 0 COMMENT '状态，0-正常 1-关闭',
+    `create_by`   bigint                                                       NULL     DEFAULT NULL COMMENT '创建者',
+    `create_time` datetime                                                     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_by`   bigint                                                       NULL     DEFAULT NULL COMMENT '更新者',
+    `update_time` datetime                                                     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `delete_time` datetime                                                              DEFAULT NULL COMMENT '删除时间',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci COMMENT ='通知公告表';
